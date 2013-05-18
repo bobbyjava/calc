95e5414f40b5f1f18d000fe4f6cf41fea74c3b22
Babak Rastgar <babakrastgar@Babaks-Mac-Pro.local>
2013-05-18 16:20:41 +0100

    Initial Commit

---
+++ calc/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  calc
+//
+//  Created by Babak Rastgar on 18/05/2013.
+//  Copyright (c) 2013 Babak Rastgar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
